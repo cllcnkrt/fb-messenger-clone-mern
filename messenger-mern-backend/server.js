2a8f70acdcc01d816c370dff3f2033a346c5f93a
cllcnkrt <cllcnkrt@outlook.com>
2021-08-12 15:35:43 +0300

    localhost 9000 retrieve conversation added

--- messenger-mern-backend/server.js
+++ messenger-mern-backend/server.js
@@ -1,30 +1,56 @@
 /* import dependencies */
-import express from 'express'
-import mongoose from 'mongoose'
-import Pusher from 'pusher'
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import Pusher from "pusher";
+import cors from "cors";
 
+import mongoMessages from "./messageModel.js";
 
 /* app config */
-const app = express()
-const port = process.env.PORT || 9000
+const app = express();
+const port = process.env.PORT || 9000;
 
 /* middlewares */
+app.use(express.json())
+app.use(cors())
 
 /* db config */
-const mongoURI = "mongodb+srv://admin:p9ji75N52LQ9rAnf@cluster0.wecq9.mongodb.net/messengerDB?retryWrites=true&w=majority"
+const mongoURI =
+  "mongodb+srv://admin:p9ji75N52LQ9rAnf@cluster0.wecq9.mongodb.net/messengerDB?retryWrites=true&w=majority";
 mongoose.connect(mongoURI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connection.once('open', () => {
-    console.log('DB CONNECTED');
-})
+mongoose.connection.once("open", () => {
+  console.log("DB CONNECTED");
+});
 
 /* api routes */
-app.get('/', (req, res) => res.status(200).send('hello world'))
+ app.get('/', (req, res) => res.status(200).send("hello world")); 
+
+app.post('/save/message', (req, res) => {
+  const dbMessage = req.body;
+ 
+  mongoMessages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get('/retrieve/conversation', (req,res)=>{
+    mongoMessages.find((err,data) =>{
+        if (err) {
+            res.status(500).send(err);
+          } else {
+            res.status(200).send(data);
+          }
+    })
+})
 
 /* listen */
-app.listen(port, () => console.log(`listenin on localhost ${port}`))
+app.listen(port, () => console.log(`listenin on localhost ${port}`));
